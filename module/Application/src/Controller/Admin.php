c7c9cf0ae49331e3cd708fd634c6a5cfaa85e724
Leo Galleguillos <leo.galleguillos@icloud.com>
2017-08-08 16:48:20 -0700

    Conditionally code Add Log Post action

--- module/Application/src/Controller/Admin.php
+++ module/Application/src/Controller/Admin.php
@@ -12,5 +12,15 @@ class Admin extends AbstractActionController
 
     public function addLogAction()
     {
+        if (!empty($_POST)) {
+            return $this->addLogPostAction();
+        }
+    }
+
+    private function addLogPostAction()
+    {
+        return $this->redirect()
+            ->toRoute('admin', ['action' => 'add-log'])
+            ->setStatusCode(303);
     }
 }
