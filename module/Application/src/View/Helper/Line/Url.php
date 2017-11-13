0a6a6eb4f4b633719384be63258323579aee03fb
Leo Galleguillos <leo.galleguillos@icloud.com>
2017-11-13 09:02:28 -0800

    Create LineUrlHelper

---
+++ module/Application/src/View/Helper/Line/Url.php
@@ -0,0 +1,17 @@
+<?php
+namespace Application\View\Helper\Line;
+
+use Application\Model\Entity\Line as LineEntity;
+use Zend\View\Helper\AbstractHelper;
+
+class Url extends AbstractHelper
+{
+    public function __invoke(string $string)
+    {
+    }
+
+    public function getRootRelativeUrl(LineEntity $lineEntity)
+    {
+
+    }
+}
